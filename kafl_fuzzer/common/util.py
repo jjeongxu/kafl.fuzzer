974a084f563144de5d7090aa846ab3107e069d66
jjeongxu <143536771+jjeongxu@users.noreply.github.com>
2025-08-04 15:27:10 +0900

    Update util.py

--- kafl_fuzzer/common/util.py
+++ kafl_fuzzer/common/util.py
@@ -277,7 +277,7 @@ class Dependency:
         if target_index == -1:
             return None
         else:
-            while True:
+            for _ in range(100)
                 num = random.choice(self.dependency[target_index])
                 if num==ioctl:
                     continue
